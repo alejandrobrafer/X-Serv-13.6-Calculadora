b1358efb34cc51d682e07f8179203c938547ddd7
Alejandro Bravo Fernández <alejandrobravofernandez@ubuntu.(none)>
2018-02-05 03:02:10 -0800

    v.2 Control argumentos

--- calculadora.py
+++ calculadora.py
@@ -8,30 +8,32 @@ import sys
 num_parametros = len(sys.argv)
 
 try:
-	if num_parametros == 4: # Hay que inluir el propio nombre del script.
+	if num_parametros == 4: 
 		if (sys.argv[1] == "sumar") or (sys.argv[1] == "SUMAR"):
 			result = float(sys.argv[2]) + float(sys.argv[3])
 			print("\n** SUMAR **")
 			print(sys.argv[2], "+", sys.argv[3], "=",  result, "\n")
 	
-		if (sys.argv[1] == "restar") or (sys.argv[1] == "RESTAR"):
+		elif (sys.argv[1] == "restar") or (sys.argv[1] == "RESTAR"):
 			result = float(sys.argv[2]) - float(sys.argv[3])
 			print("\n** RESTAR **")
 			print(sys.argv[2], "-", sys.argv[3], "=",  result, "\n")
 	
-		if (sys.argv[1] == "multiplicar") or (sys.argv[1] == "MULTIPLICAR"):
+		elif (sys.argv[1] == "multiplicar") or (sys.argv[1] == "MULTIPLICAR"):
 			result = float(sys.argv[2]) * float(sys.argv[3])
 			print("\n** MULTIPLICAR **")
 			print(sys.argv[2], "*", sys.argv[3], "=",  result, "\n")
 	
-		if (sys.argv[1] == "dividir") or (sys.argv[1] == "DIVIDIR"):
+		elif (sys.argv[1] == "dividir") or (sys.argv[1] == "DIVIDIR"):
 			try:
 				result = float(sys.argv[2]) / float(sys.argv[3])
 				print("\n** DIVIDIR **")
 				print(sys.argv[2], "/", sys.argv[3], "=",  result, "\n")
 			except ZeroDivisionError:
 				print("\nIndefinido\n")
+		else:
+			print("Segundo argumento incorrecto.\n")
 	else:
 		print("Número de argumentos incorrectos.\n")
 except ValueError:
-		print("Error en los tipos de los argumentos.\n")
+		print("Error en el valor del tercer y cuarto argumento.\n")
